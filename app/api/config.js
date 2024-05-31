d62ba051cbb6ced8cf20f4ad9c45bfbada18109d
Danil129 <149506380+Danil129@users.noreply.github.com>
2024-05-31 13:07:42 +0300

    подгатовил фронт

---
+++ app/api/config.js
@@ -0,0 +1,10 @@
+export const BASE_URL = 'http://localhost:3001/api'
+//export const BASE_URL = 'http://localhost:3001'
+
+export const endpoints = {
+  games: `${BASE_URL}/games`,
+  auth: `${BASE_URL}/auth/login`,
+  me: `${BASE_URL}/me`,
+
+}
+
